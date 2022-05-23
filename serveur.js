21ac3255351c2eb146ad3925e5b4840c48922322
wassim-slimane <wassim.slimane01@gmail.com>
2022-05-23 16:26:53 +0200

    feat(auth): check authentification added but as a try-catch not as middleware because of timeless

--- serveur.js
+++ serveur.js
@@ -30,43 +30,64 @@ app.get('/taches/:id', (req, res) => {
 
 app.post('/taches', (req, res) => {
 
-    const payload = req.body;
-    const schema = Joi.object({
-        description: Joi.string().min(3).max(50).required(),
-        faite: Joi.boolean().required(),
-    });
+    const token = req.header('x-auth-token');
+    if (!token) return res.status(401).json({erreur: "Vous devez vous connecter"})
 
-    const { value: task, error } = schema.validate(payload);
-    if (error) return res.status(400).send({ erreur: error.details[0].message });
+    try{
+        jwt.verify(token, process.env.JWT_PRIVATE_KEY)
+        const payload = req.body;
+        const schema = Joi.object({
+            description: Joi.string().min(3).max(50).required(),
+            faite: Joi.boolean().required(),
+        });
 
-    Tasks.insertOne(task);
-    
-    res.status(201).json({
-        description: task.description,
-        faite: task.faite,
-        message: "La tâche à bien été enregistrée",
-        status: "201"
-    });
+        const { value: task, error } = schema.validate(payload);
+        if (error) return res.status(400).send({ erreur: error.details[0].message });
+
+        Tasks.insertOne(task);
+        
+        res.status(201).json({
+            description: task.description,
+            faite: task.faite,
+            message: "La tâche à bien été enregistrée",
+            status: "201"
+        });
+    } catch (exc) {
+        res.status(401).json({
+            message: "Impossible de se connecter vous n'êtes pas authentifié"
+        })
+    }
 });
 
 app.put('/taches/:id', (req, res) => {
-    let id = req.params.id;
-    const payload = req.body;
-    const schema = Joi.object({
-        description: Joi.string().min(3).max(50).required(),
-        faite: Joi.boolean().required(),
-    });
-    const { value: task, error } = schema.validate(payload);
-    if (error) return res.status(400).send({ erreur: error.details[0].message });
 
-    Tasks.updateOne(id, task);
-    
-    res.status(201).json({
-        description: task.description,
-        faite: task.faite,
-        message: "La tâche à bien été modifié",
-        status: "201"
-    });
+    const token = req.header('x-auth-token');
+    if (!token) return res.status(401).json({erreur: "Vous devez vous connecter"})
+
+    try{
+        jwt.verify(token, process.env.JWT_PRIVATE_KEY)
+        let id = req.params.id;
+        const payload = req.body;
+        const schema = Joi.object({
+            description: Joi.string().min(3).max(50).required(),
+            faite: Joi.boolean().required(),
+        });
+        const { value: task, error } = schema.validate(payload);
+        if (error) return res.status(400).send({ erreur: error.details[0].message });
+
+        Tasks.updateOne(id, task);
+        
+        res.status(201).json({
+            description: task.description,
+            faite: task.faite,
+            message: "La tâche à bien été modifié",
+            status: "201"
+        });
+    } catch (exc) {
+        res.status(401).json({
+            message: "Impossible de se connecter vous n'êtes pas authentifié"
+        })
+    }
 })
 
 app.delete('/taches/:id', (req, res) => {
